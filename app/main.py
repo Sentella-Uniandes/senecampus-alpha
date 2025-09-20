41b54bb6dac2bb69590942789693e0a077906ca0
AmogusAzul <sarfxa0@gmail.com>
2025-09-20 15:37:59 -0500

    FastAPI-inclusion-in `main.py`

--- app/main.py
+++ app/main.py
@@ -1,5 +1,17 @@
+from fastapi import FastAPI
+
+app = FastAPI()
+
+@app.get("/health")
+def health():
+    return {"status": "ok"}
+
 def main():
     print("Hello from senecampus-backend!")
+    print("""
+          You are meant to execute this server with:
+          `uv run uvicorn app.main:app --reload`
+          """)
 
 
 if __name__ == "__main__":
